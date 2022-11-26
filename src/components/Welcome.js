7373d616aafc899945baa5ac4527d3561168c984
Shaikh Irfan <shaikhji1998@gmail.com>
2022-11-26 20:26:39 +0530

    new commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,8 @@
 // write code for Welcome component here
+
+export const Welcome = (prop) => {
+    return <div>
+        <h1>"Hey !"</h1>
+        <h2>"Welcome to Newton School."</h2>
+    </div>
+}
